50b32d3e9ceb298e86e6dd3bd0f716e7612d0ba3
锐哥 <2507233893@qq.com>
2016-07-14 01:02:02 +0800

    初始化项目

---
+++ Cparadise/Cparadise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cparadise
+//
+//  Created by HYRLoveQi on 16/7/14.
+//  Copyright © 2016年 HYRLoveQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
