251d3d0b1721913363cbe28792eca2da4c394c77
锐哥 <2507233893@qq.com>
2016-07-16 03:49:00 +0800

    创建了JSON解析类 HYRParse ，通过模型和MJExtension快速解析

---
+++ Cparadise/Cparadise/Utilities/HYRParse.h
@@ -0,0 +1,18 @@
+//
+//  HYRParse.h
+//  Cparadise
+//
+//  Created by HYRLoveQi on 16/7/16.
+//  Copyright © 2016年 HYRLoveQi. All rights reserved.
+/*
+ *网络数据 解析 类
+ **/
+
+#import <Foundation/Foundation.h>
+
+@interface HYRParse : NSObject
+//
++(NSMutableArray*)parseCategoryInfoWithDic:(NSDictionary *)dic;
+
+
+@end
---
+++ Cparadise/Cparadise/Utilities/HYRParse.m
@@ -0,0 +1,41 @@
+//
+//  HYRParse.m
+//  Cparadise
+//
+//  Created by HYRLoveQi on 16/7/16.
+//  Copyright © 2016年 HYRLoveQi. All rights reserved.
+//
+
+#import "HYRParse.h"
+#import "HYRCategory.h"
+#import <MJExtension.h>
+
+
+@implementation HYRParse
+
++(NSMutableArray*)parseCategoryInfoWithDic:(NSDictionary *)dic{
+    NSMutableArray * array = [NSMutableArray array];
+    NSLog(@"解析的时候%@",dic[@"result"]);
+    NSArray * tempArray = dic[@"result"];
+    
+    NSDictionary *resDict = tempArray[0];
+    
+    NSArray *resArr = resDict[@"list"];
+    
+    
+    //一句话解析json，相当于下面for循环的代码
+    [HYRCategory mj_setupReplacedKeyFromPropertyName:^NSDictionary *{
+        return @{
+                 @"categoryId" : @"id",
+                 @"categoryName" : @"name"
+                 };
+    }];
+    
+    array = [HYRCategory mj_objectArrayWithKeyValuesArray:resArr];
+    
+    return array;
+}
+
+
+
+@end
